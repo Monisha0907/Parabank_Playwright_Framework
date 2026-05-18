92a3e9e00262d1fea072eefb481c7a4c8de149b4
Monisha0907 <monisha9797@gmail.com>
2026-05-18 18:01:50 +0530

    added dynamic username and passowrd login

--- pages/Register and Login.js
+++ pages/Register and Login.js
@@ -17,23 +17,32 @@ class register
         this.confirmPassword = page.locator('[id="repeatedPassword"]');
         this.registerButton = page.getByRole('button', { name: 'Register' });
         this.successMessage = page.locator('text=Your account was created successfully. You are now logged in.');
+        this.logoutButton = page.getByRole('link', { name: 'Log Out' });
+        this.usernameLocator = page.locator('input[name="username"]');
+        this.passwordLocator = page.locator('input[name="password"]');
+        this.loginButton = page.getByRole('button', { name: 'Log In' });
     }
 
-    async registerDetails()
+    async registerDetails(username, password)
     {
         await this.firstName.click();
-        await this.firstName.fill('hello');
-        await this.lastName.fill('parabank');
+        await this.firstName.fill('Monisha');
+        await this.lastName.fill('Radja');
         await this.street.fill('xxx');
         await this.city.fill('yyy');
         await this.state.fill('xyz');
         await this.zipCode.fill('12345');
         await this.phoneNumber.fill('1234567890');
         await this.ssn.fill('456375');
-        await this.username.fill('helloparabank');
-        await this.password.fill('0987');
-        await this.confirmPassword.fill('0987');
+        await this.username.fill(username);
+        await this.password.fill(password);
+        await this.confirmPassword.fill(password);
         await this.registerButton.click();
+        await this.logoutButton.click();
+        await this.usernameLocator.fill(username);
+        await this.passwordLocator.fill(password);
+        await this.loginButton.click();
+
     }
 
    
--- tests/Authetication.spec.js
+++ tests/Authetication.spec.js
@@ -1,11 +1,18 @@
 import { test, expect } from '@playwright/test';
-import Register from '../pages/Register';
+import Register from '../pages/Register and Login';
 
 test.only('Register', async ({ page }) => {
     const registerPage = new Register(page);
+
+    const randomNumber = Math.floor(Math.random() * 100000);
+
+    const username = `monisha${randomNumber}`;
+
+    const password = 'Test@123';
+    
     await page.goto(registerPage.URL);
-    await registerPage.registerDetails();
-    await page.waitForLoadState('networkidle');
-    await expect(registerPage.successMessage).toBeVisible();
-    await page.pause();
+    await registerPage.registerDetails(username, password);
+    console.log(username);
+    console.log(password);
+
 });
